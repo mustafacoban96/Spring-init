3b5c51ea50aa0c820ccbe8757cd61fd399c03500
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-09-14 20:17:43 +0300

    created jparepo

---
+++ api/src/main/java/com/pokemonreview/api/repository/PokemonRepository.java
@@ -0,0 +1,9 @@
+package com.pokemonreview.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pokemonreview.api.model.Pokemon;
+
+public interface PokemonRepository extends JpaRepository<Pokemon, Integer>{
+
+}
