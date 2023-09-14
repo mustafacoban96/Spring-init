e9cf0cfd816c6f9bea729d1d10a73f5aadff15e3
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-09-14 23:17:44 +0300

    service layer injected contorller layer

--- api/src/main/java/com/pokemonreview/api/controllers/PokemonController.java
+++ api/src/main/java/com/pokemonreview/api/controllers/PokemonController.java
@@ -3,6 +3,7 @@ package com.pokemonreview.api.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,13 +16,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pokemonreview.api.dto.PokemonDto;
 import com.pokemonreview.api.model.Pokemon;
+import com.pokemonreview.api.service.PokemonService;
 
 @RestController
 @RequestMapping("/api/")
 public class PokemonController {
 	
+	private PokemonService pokemonService;
 	
+	@Autowired
+	public PokemonController(PokemonService pokemonService) {
+		this.pokemonService = pokemonService;
+	}
+
+
 	//ResponseEntity sınıfı HttpEntity sınıfına ek olarak builder tasarım desenini kullanarak isteğe yanıt olarak header bilgisi, HTTP durum kodu gibi bilgileri eklemeyi sağlar.
 	@GetMapping("pokemon") // => http://localhost:8080/api/pokemon
 	public ResponseEntity<List<Pokemon>> getPokemons(){
@@ -37,16 +47,28 @@ public class PokemonController {
 	public Pokemon pokemonDetail(@PathVariable int id) {
 		 return  new Pokemon(id,"Squirtle","Water");
 	}
+	/////////////////////////////////////////////////////////////////
 	
-	@PostMapping("pokemon/create")
+	/*@PostMapping("pokemon/create")
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Pokemon> createPokemon(@RequestBody Pokemon pokemon){
 		System.out.println(pokemon.getName());
 		System.out.println(pokemon.getType());
 		
 		return new ResponseEntity<>(pokemon,HttpStatus.CREATED);
+	}*/
+	
+	// injected version
+	
+	@PostMapping("pokemon/create")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<PokemonDto> createPokemon(@RequestBody PokemonDto pokemonDto){
+		return new ResponseEntity<>(pokemonService.createPoekmon(pokemonDto),HttpStatus.CREATED);
 	}
 	
+	
+	
+	///////////////////////////////////////////////////////////////////////////////////////
 	
 	@PutMapping("pokemon/{id}/update")
 	public ResponseEntity<Pokemon> updatePokemon(@RequestBody Pokemon pokemon, @PathVariable("id") int pokemonId){
