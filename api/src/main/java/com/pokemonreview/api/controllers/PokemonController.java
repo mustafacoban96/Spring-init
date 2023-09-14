610aa815f99defecec2d0cc28b9c3cf846c02f88
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-09-14 17:51:46 +0300

    crud controllers and mapping with annotatoins

---
+++ api/src/main/java/com/pokemonreview/api/controllers/PokemonController.java
@@ -0,0 +1,75 @@
+package com.pokemonreview.api.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pokemonreview.api.model.Pokemon;
+
+@RestController
+@RequestMapping("/api/")
+public class PokemonController {
+	
+	
+	//ResponseEntity sınıfı HttpEntity sınıfına ek olarak builder tasarım desenini kullanarak isteğe yanıt olarak header bilgisi, HTTP durum kodu gibi bilgileri eklemeyi sağlar.
+	@GetMapping("pokemon") // => http://localhost:8080/api/pokemon
+	public ResponseEntity<List<Pokemon>> getPokemons(){
+		List<Pokemon> pokemons = new ArrayList<>();
+		pokemons.add(new Pokemon(1, "Squirtle", "Water"));
+		pokemons.add(new Pokemon(2,"Pikachu","Electric"));
+		pokemons.add(new Pokemon(3,"Chamander","Fire"));
+		return ResponseEntity.ok(pokemons);
+	}
+	
+	
+	@GetMapping("pokemon/{id}")
+	public Pokemon pokemonDetail(@PathVariable int id) {
+		 return  new Pokemon(id,"Squirtle","Water");
+	}
+	
+	@PostMapping("pokemon/create")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Pokemon> createPokemon(@RequestBody Pokemon pokemon){
+		System.out.println(pokemon.getName());
+		System.out.println(pokemon.getType());
+		
+		return new ResponseEntity<>(pokemon,HttpStatus.CREATED);
+	}
+	
+	
+	@PutMapping("pokemon/{id}/update")
+	public ResponseEntity<Pokemon> updatePokemon(@RequestBody Pokemon pokemon, @PathVariable("id") int pokemonId){
+		System.out.println(pokemon.getName());
+		System.out.println(pokemon.getType());
+		return ResponseEntity.ok(pokemon);
+	}
+	
+	
+	@DeleteMapping("pokemon/{id}/delete")
+	public ResponseEntity<String> deletePokemon(@PathVariable("id") int pokemonId){
+		System.out.println(pokemonId);
+		return ResponseEntity.ok("Pokemon was deleted successfully");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
