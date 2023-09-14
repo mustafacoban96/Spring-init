756e9e14fda63634c2c3154d6d00632e28a2ea87
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-09-14 23:06:14 +0300

    Repository inject to service layer and Dto

--- api/src/main/java/com/pokemonreview/api/service/impl/PokemonServiceImpl.java
+++ api/src/main/java/com/pokemonreview/api/service/impl/PokemonServiceImpl.java
@@ -1,14 +1,39 @@
 package com.pokemonreview.api.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.pokemonreview.api.dto.PokemonDto;
+import com.pokemonreview.api.model.Pokemon;
+import com.pokemonreview.api.repository.PokemonRepository;
 import com.pokemonreview.api.service.PokemonService;
 
+
+@Service
 public class PokemonServiceImpl implements PokemonService{
+	
+	private PokemonRepository pokemonRepository;
+	
+	
+	@Autowired // we inject Repo -> Service
+	public PokemonServiceImpl(PokemonRepository pokemonRepository) {
+		this.pokemonRepository = pokemonRepository;
+	}
 
 	@Override
 	public PokemonDto createPoekmon(PokemonDto pokemonDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Pokemon pokemon = new Pokemon();
+		pokemon.setName(pokemon.getName());
+		pokemon.setType(pokemon.getType());
+		
+		Pokemon newPokemon = pokemonRepository.save(pokemon);
+		PokemonDto pokemonResponse = new PokemonDto();
+		
+		pokemonResponse.setId(newPokemon.getId());
+		pokemonResponse.setName(newPokemon.getName());
+		pokemonResponse.setTpye(newPokemon.getType());
+		
+		return pokemonResponse;
 	}
 
 }
