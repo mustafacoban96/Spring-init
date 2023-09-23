cd3c64e0549e2faec914c9ae6a35be2d02de361d
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-09-23 18:32:25 +0300

    model part

---
+++ web/src/main/java/com/mvcexp/web/models/Club.java
@@ -0,0 +1,37 @@
+package com.mvcexp.web.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+ @Table(name = "clubs")
+public class Club {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String title;
+	private String photoUrl;
+	private String content;
+	@CreationTimestamp
+	private LocalDateTime createOn;
+	@UpdateTimestamp
+	private LocalDateTime updateOn;
+	
+}
