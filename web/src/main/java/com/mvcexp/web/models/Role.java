583d1c33de36b8c9f29b728f54d07cc378100aac
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-09-29 16:14:47 +0300

    role-based security part user and role Entities

---
+++ web/src/main/java/com/mvcexp/web/models/Role.java
@@ -0,0 +1,29 @@
+package com.mvcexp.web.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name= "roles")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@ManyToMany(mappedBy = "roles")
+	private List<UserEntity> users = new ArrayList<>();  
+}
---
+++ web/src/main/java/com/mvcexp/web/models/UserEntity.java
@@ -0,0 +1,41 @@
+package com.mvcexp.web.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "users")
+public class UserEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String email;
+	private String password;
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+				name = "users_roles",
+				joinColumns = {@JoinColumn(name = "user_id",referencedColumnName = "id")},
+				inverseJoinColumns = {@JoinColumn(name="role_id" ,referencedColumnName = "id")}
+		)
+	private List<Role> roles = new ArrayList<>();
+}
