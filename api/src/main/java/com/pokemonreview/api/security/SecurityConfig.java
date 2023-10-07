1d2dead8250e6c206c23eb3c11e42f7ae215e795
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-10-07 18:56:23 +0300

    UserDetailService and AuthenticationManager

--- api/src/main/java/com/pokemonreview/api/security/SecurityConfig.java
+++ api/src/main/java/com/pokemonreview/api/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.pokemonreview.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,13 +21,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 	
+	
+	private CustomUserDetailsService userDetailsService;
+	
+	
+	@Autowired  // we inject CustomerDetailsService -> SecurityConfig
+	public SecurityConfig(CustomUserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		http
 			.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(auth -> auth
 					.requestMatchers(HttpMethod.GET)
-					.permitAll()
+					.authenticated()
 					.anyRequest()
 					.authenticated())
 			.httpBasic(Customizer.withDefaults());
@@ -43,5 +57,14 @@ public class SecurityConfig {
 				.build();
 		
 		return new InMemoryUserDetailsManager(admin,user);
+	}
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
