79c92e313f056a8bcccf9d31b03d966d6a87bade
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-01-29 17:52:15 +0300

    first commit

---
+++ basic-auth/src/main/java/com/shepherd/securitypractice/basicauth/models/User.java
@@ -0,0 +1,5 @@
+package com.shepherd.securitypractice.basicauth.models;
+
+public class User{
+
+}
---
+++ basic-auth/src/test/java/com/shepherd/securitypractice/basicauth/BasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shepherd.securitypractice.basicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
