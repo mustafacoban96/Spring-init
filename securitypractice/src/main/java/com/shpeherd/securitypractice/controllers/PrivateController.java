f9275a99383ec6abcaa39f7d09b418469928a83d
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-01-29 16:34:18 +0300

    using preAuthorize annotation for roles

--- securitypractice/src/main/java/com/shpeherd/securitypractice/controllers/PrivateController.java
+++ securitypractice/src/main/java/com/shpeherd/securitypractice/controllers/PrivateController.java
@@ -1,5 +1,6 @@
 package com.shpeherd.securitypractice.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/private")
 public class PrivateController {
-
+	
+	
+	
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("admin")
 	public String privateAdmin() {
 		return "Hello from private I am admin";
 	}
 	
+	@PreAuthorize("hasRole('USER')")
 	@GetMapping("user")
 	public String privateUser() {
 		return "Hello from private I am user";
